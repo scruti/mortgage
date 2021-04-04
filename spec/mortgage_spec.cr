866c816b055e0461a284bb1c6cc120ceeb7b99cb
Marc Sardon <pringaohowto@gmail.com>
2021-04-04 21:33:23 +0100

    Implement basic mortgage calculations
    Creates a mortgage based on the provided information:
    - Loan amount.
    - Annual interest rate.
    - Term.
    
    From that information it calculates:
    - Monthly payment amount.
    - Outstanding loan after a given month.
    - For a given month payment:
        - amount corresponding to principal.
        - amount corresponding to interest.

---
+++ spec/mortgage_spec.cr
@@ -0,0 +1,101 @@
+require "./spec_helper"
+
+private def mortgage
+  Mortgage.new(loan: 100_000, rate: 6.0, term: 180)
+end
+
+describe Mortgage do
+  describe "#initialize" do
+    it "builds a mortgage object with the given attributes" do
+      m = Mortgage.new(loan: 100_000.00, rate: 6.00, term: 10)
+      m.loan.should eq 100_000.00
+      m.rate.should eq 6.00
+      m.term.should eq 10
+    end
+
+    it "sets the monthly rate for the mortgage" do
+      m = Mortgage.new(loan: 100_000.00, rate: 6.00, term: 10)
+      m.monthly_rate.should eq 0.005
+    end
+
+    it "raises an error when the given interest rate is 0" do
+      expect_raises(Mortgage::InvalidRateException, "Mortage interest rate must be a positive value") do
+        Mortgage.new(loan: 100_000.00, rate: 0.00, term: 10)
+      end
+    end
+
+    it "raises an error when the given interest rate is negative" do
+      expect_raises(Mortgage::InvalidRateException, "Mortage interest rate must be a positive value") do
+        Mortgage.new(loan: 100_000.00, rate: -1.00, term: 10)
+      end
+    end
+  end
+
+  describe "#monthly_payment" do
+    it "calculates the mortgage monthly payment" do
+      mortgage.monthly_payment.should eq 843.86
+    end
+  end
+
+  describe "#outstanding_loan" do
+    it "calculates the amount left from the original loan after paying the given number of months" do
+      mortgage.outstanding_loan(month: 1).should eq 99_656.14
+    end
+
+    it "decreases between months" do
+      mortgage.outstanding_loan(1).should be > mortgage.outstanding_loan(2)
+    end
+
+    it "before the first month payment the outstansing loan is the original loan amount" do
+      mortgage.outstanding_loan(month: 0).should eq 100_000.00
+    end
+
+    it "at the last month payment there is no outstanding loan" do
+      mortgage.outstanding_loan(month: 180).should eq 0.00
+    end
+  end
+
+  describe "#interest_payment_at" do
+    it "calculates the interest part of the first month payment" do
+      mortgage.interest_payment_at(1).should eq 500
+    end
+
+    it "returns 0 when given month excedes the mortgage term" do
+      mortgage.interest_payment_at(200).should eq 0.00
+    end
+
+    it "returns 0 when the given 0 as month" do
+      mortgage.interest_payment_at(0).should eq 0.00
+    end
+
+    it "returns 0 when given a negative value as month" do
+      mortgage.interest_payment_at(-1).should eq 0.00
+    end
+
+    it "decreases between months" do
+      mortgage.interest_payment_at(1).should be > mortgage.interest_payment_at(2)
+    end
+  end
+
+  describe "#principal_payment_at" do
+    it "calculates the principal part of the first month payment" do
+      mortgage.principal_payment_at(1).should eq 343.86
+    end
+
+    it "returns 0 when given month excedes the mortgage term" do
+      mortgage.principal_payment_at(200).should eq 0.00
+    end
+
+    it "returns 0 when given 0 as month" do
+      mortgage.principal_payment_at(0).should eq 0.00
+    end
+
+    it "returns 0 when given a negative value as month" do
+      mortgage.principal_payment_at(-1).should eq 0.00
+    end
+
+    it "increases between months" do
+      mortgage.principal_payment_at(1).should be < mortgage.principal_payment_at(2)
+    end
+  end
+end
---
+++ src/mortgage.cr
@@ -0,0 +1,55 @@
+# Provides mortgage payments information based on the mortgage *loan*,
+# annual interest *rate* and *term* length.
+class Mortgage
+  property loan : Float64, rate : Float64, term : Int32
+  getter monthly_rate : Float64
+
+  class InvalidRateException < Exception
+    def message
+      "Mortage interest rate must be a positive value"
+    end
+  end
+
+  # Initializes a mortgage object.
+  # *loan*: Loan amount taken for the mortgage.
+  # *rate*: Annual fixed interest rate in percent.
+  # *term*: Number of months the morgage will last.
+  def initialize(@loan : Float64, @rate : Float64, @term : Int32)
+    raise InvalidRateException.new if rate <= 0.0
+
+    @monthly_rate = @rate / 100 / 12
+  end
+
+  # Calculates the monthly payment for the mortgage loan.
+  def monthly_payment : Float64
+    (loan * monthly_rate * ((1 + monthly_rate) ** term) /
+    ((1 + monthly_rate) ** term - 1)).round(2)
+  end
+
+  # Outstanding loan after the payment for the *month*.
+  def outstanding_loan(month : Int32) : Float64
+    (loan * ((1 + monthly_rate) ** term - (1 + monthly_rate) ** month) /
+    ((1 + monthly_rate) ** term - 1)).round(2)
+  end
+
+  # Calculates the amount of the payment for the given *month* that goes against
+  # the mortage interest.
+  def interest_payment_at(month : Int32) : Float64
+    return 0.00 unless valid_month?(month)
+
+    (outstanding_loan(month - 1) * monthly_rate).round(2)
+  end
+
+  # Calculates the amount of the payment for the given *month* that goes against
+  # the mortage principal.
+  def principal_payment_at(month : Int32) : Float64
+    return 0.00 unless valid_month?(month)
+
+    (monthly_payment - interest_payment_at(month)).round(2)
+  end
+
+  # Indicates if the given *month* belongs to the mortgage term.
+  private def valid_month?(month : Int32) : Bool
+    month >= 1 && month <= term
+  end
+end
